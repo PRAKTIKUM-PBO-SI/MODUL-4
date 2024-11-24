b04c75f3b795a2da5a17b30b66fa7b05b643a247
BangKurt <126741739+BangKurt@users.noreply.github.com>
2024-11-24 21:32:08 +0700

    Add files via upload

---
+++ Zoe Satrian_2311103141_Modul4/LatihanP9ZoeSatrian/src/LatihanP9ZoeSatrian/PraktikGUI.java
@@ -0,0 +1,59 @@
+package LatihanP9ZoeSatrian;
+// Zoe Satrian / 2311103141 -S1SI-07-C
+
+import javax.swing.*;
+
+public class PraktikGUI {
+    
+    public static void main(String[] args) {
+        
+        JFrame myFrame = new JFrame ("My First Name");
+        JOptionPane.showMessageDialog(null, "Peringatan sebentar lagi libur !",
+                "Danger", JOptionPane.WARNING_MESSAGE);
+        
+        myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        myFrame.setSize(600, 600);
+        myFrame.setVisible(true);
+        
+//        String hari[] = { "Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu", "Ahad" };
+//        
+//        JFrame myFrame = new JFrame ("My First Name");
+//        JPanel myPanel = new JPanel ();
+//        myPanel.setLayout(new BoxLayout(myPanel, BoxLayout.Y_AXIS));
+//        JLabel myLabel = new JLabel ("Label Pertama Ku...");
+//        JButton myButton = new JButton ("Tombol 1st");
+//        JButton myNdButton = new JButton ("Tombol 2nd");
+//        JTextArea myTextArea = new JTextArea ("My First Teks Area...", 5, 30);
+//        JCheckBox myCbxOne = new JCheckBox ("Libur masih lama");
+//        JCheckBox myCbxTwo = new JCheckBox ("Libur agak dekat");
+//        JCheckBox myCbxThree = new JCheckBox ("Libur sudah dekat");
+//        JCheckBox myCbxFour = new JCheckBox ("Libur sudah tiba");
+//        JRadioButton myRdoOne = new JRadioButton ("Putera", true);
+//        JRadioButton myRdoTwo = new JRadioButton ("Puteri", false);
+//        ButtonGroup grpRdo = new ButtonGroup();
+//        
+//        grpRdo.add(myRdoOne);
+//        grpRdo.add(myRdoTwo);
+//        JList daftarHari = new JList(hari);
+//        daftarHari.setSelectedIndex(0);
+//        JComboBox cbbHari = new JComboBox(hari);
+//        myPanel.add(myLabel);
+//        myPanel.add(myButton);
+//        myPanel.add(myNdButton);
+//        myPanel.add(myTextArea);
+//        myPanel.add(myCbxOne);
+//        myPanel.add(myCbxTwo);
+//        myPanel.add(myCbxThree);
+//        myPanel.add(myCbxFour);
+//        myPanel.add(myRdoOne);
+//        myPanel.add(myRdoTwo);
+//        myPanel.add(daftarHari);
+//        myPanel.add(cbbHari);
+//        myFrame.getContentPane().add(myPanel);
+//        myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//        myFrame.setSize(600, 600);
+//        myFrame.setVisible(true);
+        
+    }
+    
+}
