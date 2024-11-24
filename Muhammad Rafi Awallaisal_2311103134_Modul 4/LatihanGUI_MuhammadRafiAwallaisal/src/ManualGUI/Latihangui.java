3e4398930301e43b1620e9fa245c7d849d58d621
MUHAMMAD RAFI AWALLAISAL <rafivalorant06@gmail.com>
2024-11-25 00:09:23 +0700

    Add files via upload

---
+++ Muhammad Rafi Awallaisal_2311103134_Modul 4/LatihanGUI_MuhammadRafiAwallaisal/src/ManualGUI/Latihangui.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ManualGUI;
+
+//@author Muhammad Rafi Awallaisal 2311103134/07C
+import javax.swing.*;
+ 
+public class Latihangui {
+    public static void main(String[] args) {
+        JFrame myFrame = new JFrame ("My First Frame");
+        myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        myFrame.setSize(600,600);
+        myFrame.setVisible(true); 
+}
+} 
